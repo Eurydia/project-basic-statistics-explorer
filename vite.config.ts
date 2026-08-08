caa78facb485399cb34e16eb865237442b55d38f
Thanakorn Phuttharaksa <crossdeltavi@gmail.com>
2026-08-08 11:41:04 +0700

    added extra seo improvements for cloudflare deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/project-basic-statistics-explorer/",
+  base: "/",
   resolve: { tsconfigPaths: true },
 });
